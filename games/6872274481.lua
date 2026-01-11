7977a54b7cd5da2fd1f916cf1da5879830f0e991
b-ltdev <238304049+b-ltdev@users.noreply.github.com>
2026-01-11 17:12:15 -0500

    should work

--- games/6872274481.lua
+++ games/6872274481.lua
@@ -5419,7 +5419,7 @@ run(function()
 				end
 				task.wait(Delay.Value)
 			end
-			bedwars.Client:GetNamespace('Inventory'):Get('SetObservedChest'):SendToServer(chest.ChestFolderValue.Value)
+			bedwars.Client:GetNamespace('Inventory'):Get('SetObservedChest'):SendToServer(chest:FindFirstChild("ChestFolderValue").Value)
 		end
 	end
 
